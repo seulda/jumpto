fde6d0e124dab2c39104d1c757ea5929bace3969
seulda <amekimm@gmail.com>
2024-05-01 20:27:09 +0900

    Feat: navigation, paging 기능

--- src/main/java/com/mysite/jumpto/question/QuestionController.java
+++ src/main/java/com/mysite/jumpto/question/QuestionController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import org.springframework.data.domain.Page;
+
 import com.mysite.jumpto.answer.AnswerForm;
 
 import jakarta.validation.Valid;
@@ -25,9 +27,9 @@ public class QuestionController {
 	private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList", questionList);
+    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
+        Page<Question> paging = this.questionService.getList(page);
+        model.addAttribute("paging", paging);
         return "question_list";
     }
     
--- src/main/java/com/mysite/jumpto/question/QuestionService.java
+++ src/main/java/com/mysite/jumpto/question/QuestionService.java
@@ -1,9 +1,15 @@
 package com.mysite.jumpto.question;
 
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.time.LocalDateTime;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.mysite.jumpto.DataNotFoundException;
@@ -18,6 +24,12 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+    public Page<Question> getList(int page) {
+    	List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
     
     public Question getQuestion(Integer id) {  

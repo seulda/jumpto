012c916e3fb8a8e14489e19dbed5bc207ffc9e2c
seulda <amekimm@gmail.com>
2024-05-05 00:36:50 +0900

    Feat: question, answer 의 수정/삭제 기능

--- src/main/java/com/mysite/jumpto/answer/AnswerService.java
+++ src/main/java/com/mysite/jumpto/answer/AnswerService.java
@@ -1,5 +1,8 @@
 package com.mysite.jumpto.answer;
 
+import java.util.Optional;
+
+import com.mysite.jumpto.DataNotFoundException;
 import com.mysite.jumpto.question.Question;
 import com.mysite.jumpto.user.SiteUser;
 import lombok.RequiredArgsConstructor;
@@ -22,5 +25,24 @@ public class AnswerService {
         answer.setAuthor(author);
         this.answerRepository.save(answer);
         return answer;
+    }
+    
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+    
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
